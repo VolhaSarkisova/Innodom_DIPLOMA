ccdfdcff51546a1eb1f6af1b9e7373a6744a2ed9
VolhaSarkisova <olkavalkiria@gmial.com>
2023-09-13 11:03:32 +0300

    -feat: add model Room

--- hotels/models.py
+++ hotels/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from countries.models import City
+from countries.models import City, Currency
 
 NUMBER_OF_STARS = (
     ('1', '1'),
@@ -8,6 +8,21 @@ NUMBER_OF_STARS = (
     ('3', '3'),
     ('4', '4'),
     ('5', '5'),
+)
+ROOM_CATEGORY = (
+    ('Standard room', 'Standard room'),
+    ('Improved room', 'Improved room'),
+    ('Suite room', 'Suite room'),
+    ('Deluxe room', 'Deluxe room'),
+    ('Presidential suite', 'Presidential suite'),
+)
+NUMBER_OF_SEATS = (
+    ('single room', 'single room'),
+    ('double room', 'double room'),
+    ('triple room', 'triple room'),
+    ('quadruple room', 'quadruple room'),
+    ('five-bed room', 'five-bed room'),
+    ('six-bed room', 'six-bed room')
 )
 class Hotel(models.Model):
     name = models.CharField(max_length=100,
@@ -43,3 +58,31 @@ class HotelPhotos(models.Model):
     class Meta:
         verbose_name_plural = 'Photos of hotels'
         ordering = ['hotel']
+
+class Room(models.Model):
+    hotel = models.ForeignKey(Hotel,
+                              on_delete=models.CASCADE,
+                              related_name="room_hotel")
+    number = models.CharField(max_length=4,
+                              verbose_name='Room number',
+                              help_text='Enter a room number')
+    category = models.CharField(max_length=20,
+                                choices=ROOM_CATEGORY,
+                                default='Standard room',
+                                verbose_name='Room category',
+                                help_text='Enter a room category')
+    number_of_seats = models.CharField(max_length=15,
+                                       choices=NUMBER_OF_SEATS,
+                                       default='single room')
+    price = models.DecimalField(max_digits=15,
+                                decimal_places=2)
+    currency = models.ForeignKey(Currency,
+                                 on_delete=models.PROTECT,
+                                 related_name='room_currency')
+
+    def __str__(self):
+        return f'{self.hotel}: {self.number}'
+
+    class Meta:
+        verbose_name_plural = 'Rooms'
+        ordering = ['hotel', 'number']
