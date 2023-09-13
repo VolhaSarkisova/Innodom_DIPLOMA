807031388fd296dad6e966da56c7d1c2a7d7a1bf
VolhaSarkisova <olkavalkiria@gmial.com>
2023-09-13 10:48:51 +0300

    -feat: add model City

--- countries/models.py
+++ countries/models.py
@@ -11,3 +11,18 @@ class Country(models.Model):
     class Meta:
         verbose_name_plural = 'Countries'
         ordering = ['name']
+
+class City(models.Model):
+    # Bangkok's full name is the longest
+    name = models.CharField(max_length=168,
+                            verbose_name="Сity name",
+                            help_text="Enter a city name")
+    country = models.ForeignKey(Country,
+                                on_delete=models.PROTECT,
+                                related_name='city_country')
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Cities'
+        ordering = ['name']
