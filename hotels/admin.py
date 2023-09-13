fc9290c7d0e2b684c0934a697e8e82d776f26622
VolhaSarkisova <olkavalkiria@gmial.com>
2023-09-13 11:54:32 +0300

    -feat: model registration

--- hotels/admin.py
+++ hotels/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from hotels.models import Hotel, HotelPhotos, Room, RoomPhotos
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'address', 'number_of_stars')
+    search_fields = ('name', )
+    list_filter = ('name', )
+
+@admin.register(HotelPhotos)
+class HotelPhotosAdmin(admin.ModelAdmin):
+    list_display = ('hotel', 'photo')
+    search_fields = ('hotel', )
+    list_filter = ('hotel', )
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('hotel', 'number', 'category', 'number_of_seats', 'price', 'currency')
+    search_fields = ('hotel', )
+    list_filter = ('hotel', )
+
+@admin.register(RoomPhotos)
+class RoomPhotosAdmin(admin.ModelAdmin):
+    list_display = ('room', 'photo')
+    search_fields = ('room', )
+    list_filter = ('room', )
