82ab429c2eab5f85b5d69cfa8667fa0c426bc282
VolhaSarkisova <olkavalkiria@gmial.com>
2023-09-13 13:30:00 +0300

    -fix: drop search_fields for Photos

--- hotels/admin.py
+++ hotels/admin.py
@@ -12,17 +12,15 @@ class HotelAdmin(admin.ModelAdmin):
 @admin.register(HotelPhotos)
 class HotelPhotosAdmin(admin.ModelAdmin):
     list_display = ('hotel', 'photo')
-    search_fields = ('hotel', )
     list_filter = ('hotel', )
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('hotel', 'number', 'category', 'number_of_seats', 'price', 'currency')
-    search_fields = ('hotel', )
-    list_filter = ('hotel', )
+    search_fields = ('number_of_seats', )
+    list_filter = ('hotel', 'number', 'category', 'number_of_seats')
 
 @admin.register(RoomPhotos)
 class RoomPhotosAdmin(admin.ModelAdmin):
     list_display = ('room', 'photo')
-    search_fields = ('room', )
     list_filter = ('room', )
