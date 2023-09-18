193e5c643ce571bf2b69e5cb2c25f97f5f70da6c
VolhaSarkisova <olkavalkiria@gmial.com>
2023-09-18 10:44:37 +0300

    -feat: add search to country_detail, city_detail

--- apps/countries/views.py
+++ apps/countries/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
@@ -7,7 +8,10 @@ from django.views.generic import (
     UpdateView,
     DeleteView
 )
-from apps.countries.models import Country
+from rest_framework.generics import get_object_or_404
+
+from apps.countries.models import Country, City, CountryPhotos, CityPhotos
+from apps.hotels.models import Hotel
 
 class CountryList(LoginRequiredMixin, ListView):
     model = Country
@@ -26,9 +30,47 @@ class CountryList(LoginRequiredMixin, ListView):
         context['search-input'] = search_input
         return context
 
-class CountryDetail(LoginRequiredMixin, DetailView):
-    model = Country
-    context_object_name = 'country'
+# class CountryDetail(LoginRequiredMixin, DetailView):
+#     model = Country
+#     context_object_name = 'country'
+def country_detail(request, pk):
+    country = get_object_or_404(Country, pk=pk)
+    cities = City.objects.filter(country=pk)
+    country_photos = CountryPhotos.objects.filter(country=pk)[0:3]
+
+    context = {
+        'country': country,
+        'cities': cities,
+        'country_photos': country_photos
+    }
+
+    search_city = request.GET.get('search-area') or ''
+    if search_city:
+        context['cities'] = context['cities'].filter(
+            name__icontains=search_city
+        )
+
+    return render(request, 'countries/country_detail.html', context)
+
+def city_detail(request, pk):
+    city = get_object_or_404(City, pk=pk)
+    hotels = Hotel.objects.filter(city=pk)
+    city_photos = CityPhotos.objects.filter(city=pk)
+
+    context = {
+        'city': city,
+        'hotels': hotels,
+        'city_photos': city_photos
+    }
+
+
+    search_hotel = request.GET.get('search-area') or ''
+    if search_hotel:
+        context['hotels'] = context['hotels'].filter(
+            name__icontains=search_hotel
+        )
+
+    return render(request, 'countries/city_detail.html', context)
 
 class CountryCreate(LoginRequiredMixin, CreateView):
     model = Country
@@ -37,7 +79,7 @@ class CountryCreate(LoginRequiredMixin, CreateView):
     template_name = "countries/country_form.html"
 
     def form_valid(self, form):
-        # form.instance.user = self.request.user
+        form.instance.user = self.request.user
         return super(CountryCreate, self).form_valid(form)
 
 class CountryUpdate(LoginRequiredMixin, UpdateView):
