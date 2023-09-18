b68cacf549d9296c1447f06b57ea262f07a7708d
VolhaSarkisova <olkavalkiria@gmial.com>
2023-09-18 16:55:18 +0300

    -feat: comment

--- apps/reservations/views.py
+++ apps/reservations/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from apps.reservations.models import Reservation
+
+
+# class ReservationProcess(LoginRequiredMixin, ListView):
+#     model = Reservation
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         search_
+
