c896861f014207d4dc6c9fa1d88caf12a80faf41
VolhaSarkisova <olkavalkiria@gmial.com>
2023-09-18 10:46:57 +0300

    -feat: add hotel

--- apps/router.py
+++ apps/router.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('apps.countries.urls')),
     path('user/', include('apps.users.urls')),
+    path('hotel/', include('apps.hotels.urls')),
 ]
