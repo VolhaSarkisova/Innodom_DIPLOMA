74f5bac5ca4b48a75b216db39b1a3fd0b73bd170
VolhaSarkisova <olkavalkiria@gmial.com>
2023-09-18 10:45:39 +0300

    -fix: url change

--- apps/countries/urls.py
+++ apps/countries/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path
 from apps.countries.views import (
     CountryList,
-    CountryDetail,
+    # CountryDetail,
     CountryCreate,
     CountryUpdate,
     CountryDelete,
+    country_detail,
+    city_detail,
 )
 
 urlpatterns = [
     path('countries/', CountryList.as_view(), name='countries'),
-    path('country-detail/<int:pk>/', CountryDetail.as_view(), name='country'),
+    # path('country-detail/<int:pk>/', CountryDetail.as_view(), name='country'),
+    path('country-detail/<int:pk>/', country_detail, name='country'),
     path('country-create/', CountryCreate.as_view(), name='country_create'),
     path('country-update/<int:pk>/', CountryUpdate.as_view(), name='country_update'),
-    path('country-delete/<int:pk>/', CountryDelete.as_view(), name='country_delete')
+    path('country-delete/<int:pk>/', CountryDelete.as_view(), name='country_delete'),
+
+    path('city-detail/<int:pk>/', city_detail, name='city'),
 ]
