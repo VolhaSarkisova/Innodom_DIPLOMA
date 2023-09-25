7120d547167342cb535804c7362b2b7c8f734b08
VolhaSarkisova <olkavalkiria@gmial.com>
2023-09-25 11:27:33 +0300

    -fix: drop path

--- apps/countries/urls.py
+++ apps/countries/urls.py
@@ -1,21 +1,12 @@
 from django.urls import path
 from apps.countries.views import (
     CountryList,
-    # CountryDetail,
-    CountryCreate,
-    CountryUpdate,
-    CountryDelete,
     country_detail,
     city_detail,
 )
 
 urlpatterns = [
     path('', CountryList.as_view(), name='countries'),
-    # path('country-detail/<int:pk>/', CountryDetail.as_view(), name='country'),
     path('country-detail/<int:pk>/', country_detail, name='country'),
-    path('country-create/', CountryCreate.as_view(), name='country_create'),
-    path('country-update/<int:pk>/', CountryUpdate.as_view(), name='country_update'),
-    path('country-delete/<int:pk>/', CountryDelete.as_view(), name='country_delete'),
-
     path('city-detail/<int:pk>/', city_detail, name='city'),
 ]
