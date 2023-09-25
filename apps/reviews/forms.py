ccc65c8fdacc950d6ab14c9bd3ab9c4a89303ef8
VolhaSarkisova <olkavalkiria@gmial.com>
2023-09-25 11:30:33 +0300

    -fix: drop comment

--- apps/reviews/forms.py
+++ apps/reviews/forms.py
@@ -4,10 +4,6 @@ from .models import Review
 INPUT_CLASSES = "w-full py-4 px-6 rounded-xl border"
 
 class ReviewForm(forms.ModelForm):
-    # def __init__(self, hotel_id, username, *args, **kwargs):
-    #     super(ReviewForm, self).__init__(*args, **kwargs)
-    #     self.fields['hotel'].queryset = Hotel.objects.filter(pk=hotel_id)
-    #     self.fields['user'].queryset = User.objects.filter(username=username)
     class Meta:
         model = Review
         fields = ['grade', 'comment']
