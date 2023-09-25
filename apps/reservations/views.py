cdb4262fab3a444c9f9767117475da0955581a4d
VolhaSarkisova <olkavalkiria@gmial.com>
2023-09-25 10:53:33 +0300

    -fix: search in reservation

--- apps/reservations/views.py
+++ apps/reservations/views.py
@@ -1,51 +1,36 @@
-import datetime
-
+from datetime import datetime, timedelta
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from rest_framework import status
 from rest_framework.generics import get_object_or_404
 
-from apps.hotels.models import Room
+from apps.hotels.models import Room, Hotel
 from apps.reservations.forms import ReservationCreateForm
 from apps.reservations.models import Reservation
 
-
-# @login_required()
-# def reservation_detail(request, pk):
-#     room = Room.objects.filter(pk=pk)
-#     print(request.GET.get('params'))
-#     if request.method == "POST":
-#         form = ReservationCreateForm(request.POST, request.FILES)
-#         reservation = form.save(commit=False)
-#         reservation.room_id = pk
-#         reservation.user_id = request.user.id
-#
-#         if form.is_valid():
-#             reservation.save()
-#             if request.user.is_superuser:
-#                 return redirect("countries")
-#             else:
-#                 return redirect("user_reservations")
-#     else:
-#         form = ReservationCreateForm()
-#
-#     context = {
-#         'form': form,
-#         'room': room
-#     }
-#
-#     return render(request, "reservations/reservation_form.html", context)
-
 @login_required()
 def user_reservations(request):
+    search_date_start = request.GET.get('search-date-start') or ''
+    search_date_end = request.GET.get('search-date-end') or ''
+
     if request.user.is_superuser:
         reservations = Reservation.objects.all()
     else:
         reservations = Reservation.objects.filter(user=request.user)
 
+    if search_date_start != '' and search_date_end != '':
+        date_start = datetime.strptime(search_date_start, '%Y-%m-%d')
+        date_end = datetime.strptime(str(search_date_end), '%Y-%m-%d')
+        if date_start <= date_end:
+            reservations = Reservation.objects.filter(date__range=(date_start, date_end))
+
+    rooms = Room.objects.filter(id__in=[reservation.room.id for reservation in reservations])
+    hotels = Hotel.objects.filter(id__in=[room.hotel.id for room in rooms])
+
     context = {
         'reservations': reservations,
+        'hotels': hotels,
     }
-
     return render(request, "reservations/user_reservations.html", context)
 
 @login_required()
@@ -59,16 +44,15 @@ def delete_user_reservation(request, pk):
 @login_required()
 def reservation_detail(request, pk, url):
 
-    date_start = datetime.datetime.strptime(url[(url.index('search-date-start')+len('search-date-start')+1):(url.index('search-date-start')+len('search-date-start')+1+10)], '%Y-%m-%d').date()
-    date_end = datetime.datetime.strptime(url[(url.index('search-date-end')+len('search-date-end')+1):(url.index('search-date-end')+len('search-date-end')+1+10)], '%Y-%m-%d').date()
-    print(date_start, date_end)
+    date_start = datetime.strptime(url[(url.index('search-date-start')+len('search-date-start')+1):(url.index('search-date-start')+len('search-date-start')+1+10)], '%Y-%m-%d').date()
+    date_end = datetime.strptime(url[(url.index('search-date-end')+len('search-date-end')+1):(url.index('search-date-end')+len('search-date-end')+1+10)], '%Y-%m-%d').date()
+
     reserved = Reservation.objects.filter(date__range=(date_start, date_end))
     flag = True
     for item in reserved:
-        print(item.room)
+
         if item.room.id == pk:
             flag = False
-            print(flag)
             break
 
     if flag:
@@ -79,12 +63,12 @@ def reservation_detail(request, pk, url):
             reservation.room_id = pk
             reservation.user_id = request.user.id
             reservation.date = date
-            date += datetime.timedelta(days=1)
+            date += timedelta(days=1)
             reservation.save()
-        return redirect("reservation_info", pk, date_start, date_end, 'успешно')
+        return redirect("reservation_info", pk, date_start, date_end, 'successfully')
         # redirect("user_reservations")
     else:
-        return redirect("reservation_info", pk, date_start, date_end, 'запрещено. Повторное бронирование номера невозможно.')
+        return redirect("reservation_info", pk, date_start, date_end, 'booking error')
 
 @login_required()
 def reservation_info(request, pk, date_start, date_end, info):
