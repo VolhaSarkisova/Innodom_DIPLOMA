665430a068fd7aaf080996c27c1f1bda6d53fd22
VolhaSarkisova <olkavalkiria@gmial.com>
2023-09-25 11:26:38 +0300

    -feat: add filter

--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 from datetime import datetime, date
 
-from django.db.models import Q
 from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
@@ -31,8 +30,7 @@ class HotelsFreeListAPIView(APIView):
                 rooms = rooms.exclude(id__in=[reservation.room.id for reservation in reservations])
                 hotels = hotels.filter(id__in=[room.hotel.id for room in rooms])
 
-        if hotels:
-            serializer = HotelSerializer(hotels, many=True)
+        serializer = HotelSerializer(hotels, many=True)
 
         return Response(serializer.data)
 
@@ -47,15 +45,14 @@ class RoomsFreeListAPIView(APIView):
         search_date_start = self.request.query_params.get('search-date-start')
         search_date_end = self.request.query_params.get('search-date-end')
 
-        if search_date_start and search_date_end:
+        if search_date_start and search_date_end and hotel_id:
             date_start = datetime.strptime(search_date_start, '%Y-%m-%d')
             date_end = datetime.strptime(str(search_date_end), '%Y-%m-%d')
             date_now = datetime.strptime(str(date.today()), '%Y-%m-%d')
             if date_now <= date_start <= date_end:
-                reservations = reservations.filter(Q(date__range=(date_start, date_end)))
+                reservations = reservations.filter(date__range=(date_start, date_end))
                 rooms = rooms.exclude(id__in=[reservation.room.id for reservation in reservations])
-                if hotel_id:
-                    rooms = rooms.filter(Q(hotel=hotel_id))
+                rooms = rooms.filter(hotel=hotel_id)
 
         if rooms:
             serializer = RoomSerializer(rooms, many=True)
@@ -70,7 +67,7 @@ class UserReservsListAPIView(APIView):
         # reservations = Reservation.objects.filter(Q(user=user))
 
         user = self.request.user.id
-        reservations = Reservation.objects.filter(Q(user=user))
+        reservations = Reservation.objects.filter(user=user)
 
         serializer = ReservationSerializer(reservations, many=True)
 
@@ -79,13 +76,11 @@ class UserReservsListAPIView(APIView):
 class UserAddReservationAPIView(APIView):
     # http://127.0.0.1:8010/api/user_add_reservation/
     def post(self, request: Request):
-        reservations = Reservation.objects.all()
-        rooms = Room.objects.all()
         serializer = ReservationSerializer(data=request.data)
 
         if serializer.is_valid():
-            reservations = reservations.filter(date=request.data.get('date'))
-            rooms = rooms.exclude(id__in=[reservation.room.id for reservation in reservations])
+            reservations = Reservation.objects.filter(date=request.data.get('date'))
+            rooms = Room.objects.exclude(id__in=[reservation.room.id for reservation in reservations])
 
             if request.data.get('room') in [room.id for room in rooms]:
                 serializer.save()
